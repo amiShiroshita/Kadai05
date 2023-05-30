9407601e9a0e26a7b820fa2f3949a993f2c24166
TaigaHashimoto <taiga.hashimoto@salto.link>
2023-05-30 09:21:41 +0900

    ルーティング作成

--- Controllers/LoginController.php
+++ Controllers/LoginController.php
@@ -1,1 +1,11 @@
 <?php
+
+class Login {
+    public function index():void {
+        echo "Login->index";
+    }
+
+    public function fuga():void {
+        echo "Login->fuga";
+    }
+}
--- public/index.php
+++ public/index.php
@@ -1,2 +1,37 @@
 <?php
 // ルーティング
+ini_set('display_errors', true);
+
+try {
+    $actionName = 'index';
+    $controllerName = 'LoginController';
+    
+    // 不正ログイン対策
+    if(!array_key_exists('REQUEST_URI', $_SERVER)) {
+        exit("REQUEST_URI がありません");
+    }
+
+    $urlPath = explode("/", trim(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH), "/"));
+
+    if(reset($urlPath) != "") {
+        $controllerName = sprintf("%sController", ucfirst(current($urlPath)));
+    }
+
+    if(next($urlPath)) {
+        $actionName = current($urlPath);
+    }
+
+    $fileName = dirname(__DIR__).sprintf("/Controllers/%s.php", $controllerName);
+    // 対象ファイル存在チェック
+    if(!file_exists($fileName)) {
+        exit($fileName." がありません");
+    }
+
+    require_once realpath($fileName);
+    $className = str_replace("Controller", "", $controllerName);
+    (new $className)->$actionName();
+
+} catch (Exception $e) {
+    var_dump($e->__toString());
+    exit();
+}
